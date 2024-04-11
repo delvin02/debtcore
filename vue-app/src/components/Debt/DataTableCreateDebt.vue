b9ffe53c8ddb39d611a9d56c7d5668c1bfd77eba
weeiwaai <weeiwaai02@gmail.com>
2024-04-11 10:30:31 +0930

    changes

--- vue-app/src/components/Debt/DataTableCreateDebt.vue
+++ vue-app/src/components/Debt/DataTableCreateDebt.vue
@@ -12,7 +12,7 @@ import { Input } from '@/components/ui/input'
 import { Calendar } from '@/components/ui/calendar'
 import { Label } from '@/components/ui/label'
 import { Button } from '@/components/ui/button'
-import { ref, reactive, inject, watch } from 'vue'
+import { ref, reactive, inject, watch, computed } from 'vue'
 import { cn } from '@/lib/utils'
 import {
 	Command,
@@ -24,17 +24,17 @@ import {
 } from '@/components/ui/command'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 import {
-    Select,
-    SelectContent,
-    SelectGroup,
-    SelectItem,
-    SelectLabel,
-    SelectTrigger,
-    SelectValue
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectLabel,
+	SelectTrigger,
+	SelectValue
 } from '@/components/ui/select'
 import Separator from '../ui/separator/Separator.vue'
 import axios from 'axios'
-import { format } from 'date-fns';
+import { format, parseISO } from 'date-fns'
 import { useTableStore } from '@/store/table'
 import type { Task } from '@/components/Company/data/schema'
 import { useToast } from '@/components/ui/toast/use-toast'
@@ -46,13 +46,13 @@ const tableStore = inject('tableStore', useTableStore('debt'))
 interface Debt {
 	invoice?: string
 	customer?: number | null
-	amount?: number | null
+	amount?: number | string | null
 	due_date: Date | null
 	status: number | null
 	document: File | null
 }
 
-const form = reactive<Debt>({	
+const form = reactive<Debt>({
 	invoice: '',
 	customer: null,
 	amount: null,
@@ -101,17 +101,14 @@ async function fetchCountries(query?: string) {
 async function fetchStatuses() {
 	customers.is_loading = true
 	try {
-		const response = await axios.get(
-			`http://127.0.0.1:8000/api/status/list`,
-			{
-				withCredentials: true,
-				headers: {
-					'Cache-Control': 'no-cache',
-					Pragma: 'no-cache',
-					Expires: '0'
-				}
+		const response = await axios.get(`http://127.0.0.1:8000/api/status/list`, {
+			withCredentials: true,
+			headers: {
+				'Cache-Control': 'no-cache',
+				Pragma: 'no-cache',
+				Expires: '0'
 			}
-		)
+		})
 
 		statuses.data = response.data.Result
 	} catch (error) {
@@ -140,8 +137,7 @@ function validateForm() {
 		{ condition: form.invoice === '', message: 'Invoice cannot be blank' },
 		{ condition: form.due_date?.toString == null, message: 'Due Date cannot be blank' },
 		{ condition: form.status == null, message: 'Status cannot be blank' },
-		{ condition: form.customer == null, message: 'Customer cannot be blank' },
-
+		{ condition: form.customer == null, message: 'Customer cannot be blank' }
 	]
 
 	for (let validation of validations) {
@@ -205,10 +201,9 @@ async function submit() {
 function toggleDialog() {
 	is_dialog_open.value = !is_dialog_open.value
 	if (is_dialog_open.value) {
-		 fetchCountries(searchCustomerQuery.value)
-		 fetchStatuses()
+		fetchCountries(searchCustomerQuery.value)
+		fetchStatuses()
 	}
-
 }
 
 function handleCustomerSelect(customer: any) {
@@ -222,10 +217,19 @@ function handleStatusSelect(status: any) {
 }
 
 function handleFileChange(event: Event) {
-    const input = event.target as HTMLInputElement;
-    if (input.files?.length) {
-        form.document = input.files[0]; // Assign the first selected file
-    }
+	const input = event.target as HTMLInputElement
+	if (input.files?.length) {
+		form.document = input.files[0] // Assign the first selected file
+	}
+}
+
+function updateDueDate(payload: DatePickerModel | undefined) {
+	console.log(payload)
+	if (payload?.date) {
+		form.due_date = payload.date
+	} else {
+		form.due_date = null
+	}
 }
 </script>
 
@@ -253,9 +257,12 @@ function handleFileChange(event: Event) {
 				<!-- :validation-schema="vendorSchema" -->
 				<div class="grid gap-4 py-4">
 					<div class="grid grid-cols-4 items-center gap-4">
-						<Label for="invoice" class="text-right"> Invoice
-							<span class="absolute translate-x-1/2 -translate-y-1/2 w-1.5 h-1.5 text-red-500 rounded-full">*</span>
-
+						<Label for="invoice" class="text-right">
+							Invoice
+							<span
+								class="absolute translate-x-1/2 -translate-y-1/2 w-1.5 h-1.5 text-red-500 rounded-full"
+								>*</span
+							>
 						</Label>
 						<Input
 							id="invoice"
@@ -265,8 +272,12 @@ function handleFileChange(event: Event) {
 						/>
 					</div>
 					<div class="grid grid-cols-4 items-center gap-4">
-						<Label for="customer" class="text-right leading-normal"> Customer
-							<span class="absolute translate-x-1/2 -translate-y-1/2 w-1.5 h-1.5 text-red-500 rounded-full">*</span>
+						<Label for="customer" class="text-right leading-normal">
+							Customer
+							<span
+								class="absolute translate-x-1/2 -translate-y-1/2 w-1.5 h-1.5 text-red-500 rounded-full"
+								>*</span
+							>
 						</Label>
 						<div class="col-span-3">
 							<Popover v-model:open="customers.is_open">
@@ -333,9 +344,12 @@ function handleFileChange(event: Event) {
 						</div>
 					</div>
 					<div class="grid grid-cols-4 items-center gap-4">
-						<Label for="amount" class="text-right"> Amount
-							<span class="absolute translate-x-1/2 -translate-y-1/2 w-1.5 h-1.5 text-red-500 rounded-full">*</span>
-
+						<Label for="amount" class="text-right">
+							Amount
+							<span
+								class="absolute translate-x-1/2 -translate-y-1/2 w-1.5 h-1.5 text-red-500 rounded-full"
+								>*</span
+							>
 						</Label>
 						<Input
 							id="amount"
@@ -345,99 +359,115 @@ function handleFileChange(event: Event) {
 						/>
 					</div>
 					<div class="grid grid-cols-4 items-center gap-4">
-							<Label for="email" class="text-right"> Due Date </Label>
-							<div class="col-span-3">
-									<Popover>
-											<PopoverTrigger as-child>
-													<Button
-															:variant="'outline'"
-															:class="
-																	cn(
-																			'w-full justify-start text-left font-normal',
-																			!form.due_date && 'text-muted-foreground'
-																	)
-															"
-													>
-															<VIcon name="fa-calendar" class="mr-2 h-4 w-4" />
-															<span>{{ form.due_date ? format(form.due_date, 'PPP') : 'Pick a date' }}</span>
-													</Button>
-											</PopoverTrigger>
-											<PopoverContent class="w-auto p-0">
-												<Calendar :model-value="form.due_date" @change="date => form.due_date = date" />
-											</PopoverContent>
-									</Popover>
-							</div>
+						<Label for="email" class="text-right"> Due Date </Label>
+						<div class="col-span-3">
+							<Popover>
+								<PopoverTrigger as-child>
+									<Button
+										:variant="'outline'"
+										:class="
+											cn(
+												'w-full justify-start text-left font-normal',
+												!form.due_date && 'text-muted-foreground'
+											)
+										"
+									>
+										<VIcon name="fa-calendar" class="mr-2 h-4 w-4" />
+										<span>{{
+											form.due_date
+												? format(form.due_date, 'yyyy-MM-dd')
+												: 'Pick a date'
+										}}</span>
+									</Button>
+								</PopoverTrigger>
+								<PopoverContent class="w-auto p-0">
+									<Calendar
+										v-model="form.due_date"
+										@update:model-value="console.log('test')"
+									></Calendar>
+								</PopoverContent>
+							</Popover>
+						</div>
 					</div>
 					<div class="grid grid-cols-4 items-center gap-4">
-							<Label for="mobile" class="text-right"> Status </Label>
-							<div class="col-span-3">
-								<Popover v-model:open="statuses.is_open">
-									<PopoverTrigger as-child>
-										<Button
-											variant="outline"
-											role="combobox"
-											:aria-expanded="statuses.is_open"
-											class="w-full justify-between px-3"
-											:disabled="statuses.is_loading"
-										>
-											{{
-												form.status
-													? statuses.data.find(
-															(status) => status.id === form.status
-														)?.label
-													: 'Select status'
-											}}
-											<VIcon
-												name="fa-circle-notch"
-												v-if="statuses.is_loading"
-												animation="spin"
-												class="w-4 h-4 mr-2"
-											/>
-											<VIcon
-												v-else
-												name="fa-angle-down"
-												class="h-4 w-4 shrink-0 opacity-50"
-											/>
-										</Button>
-									</PopoverTrigger>
-									<PopoverContent class="w-[500px] p-1">
-										<Command>
-											<CommandList>
-												<CommandGroup>
-													<CommandItem
-														v-for="status in statuses.data"
-														:key="status.id"
-														:value="status.value ?? ''"
-														@select="() => handleStatusSelect(status)"
-													>
-														{{ status.label }}
-														<VIcon
-															name="fa-check"
-															:class="[
-																'ml-auto h-4 w-4',
-																form.status === status.id
-																	? 'opacity-100'
-																	: 'opacity-0'
-															]"
-														/>
-													</CommandItem>
-												</CommandGroup>
-											</CommandList>
-										</Command>
-									</PopoverContent>
-								</Popover>
-							</div>
+						<Label for="mobile" class="text-right"> Status </Label>
+						<div class="col-span-3">
+							<Popover v-model:open="statuses.is_open">
+								<PopoverTrigger as-child>
+									<Button
+										variant="outline"
+										role="combobox"
+										:aria-expanded="statuses.is_open"
+										class="w-full justify-between px-3"
+										:disabled="statuses.is_loading"
+									>
+										{{
+											form.status
+												? statuses.data.find(
+														(status) => status.id === form.status
+													)?.label
+												: 'Select status'
+										}}
+										<VIcon
+											name="fa-circle-notch"
+											v-if="statuses.is_loading"
+											animation="spin"
+											class="w-4 h-4 mr-2"
+										/>
+										<VIcon
+											v-else
+											name="fa-angle-down"
+											class="h-4 w-4 shrink-0 opacity-50"
+										/>
+									</Button>
+								</PopoverTrigger>
+								<PopoverContent class="w-[500px] p-1">
+									<Command>
+										<CommandList>
+											<CommandGroup>
+												<CommandItem
+													v-for="status in statuses.data"
+													:key="status.id"
+													:value="status.value ?? ''"
+													@select="() => handleStatusSelect(status)"
+												>
+													{{ status.label }}
+													<VIcon
+														name="fa-check"
+														:class="[
+															'ml-auto h-4 w-4',
+															form.status === status.id
+																? 'opacity-100'
+																: 'opacity-0'
+														]"
+													/>
+												</CommandItem>
+											</CommandGroup>
+										</CommandList>
+									</Command>
+								</PopoverContent>
+							</Popover>
+						</div>
 					</div>
 					<Separator />
 					<div class="grid grid-cols-4 items-center gap-x-4 text-primary">
-							<Label for="mobile" class="text-right"> Attachment </Label>
-							<div class="col-span-3 ">
-									<Input id="name" type="file" @change="handleFileChange" class="col-span-3" />
-							</div> 
-							<p class="col-start-2 col-span-3 mt-1 text-sm text-gray-500 dark:text-gray-300">SVG, PNG, JPG or JPEG, PDF (MAX. 1080x720px).</p>
+						<Label for="mobile" class="text-right"> Attachment </Label>
+						<div class="col-span-3">
+							<Input
+								id="name"
+								type="file"
+								@change="handleFileChange"
+								class="col-span-3"
+							/>
+						</div>
+						<p
+							class="col-start-2 col-span-3 mt-1 text-sm text-gray-500 dark:text-gray-300"
+						>
+							SVG, PNG, JPG or JPEG, PDF (MAX. 1080x720px).
+						</p>
 					</div>
 				</div>
-				<Separator/>
+				<Separator />
 				<div class="grid grid-cols-4 items-center gap-4" v-if="error_message">
 					<Label for="name" class="text-red-600 col-span-3 col-start-2">
 						<VIcon name="fa-exclamation-triangle" class="size-4 fill-red-600" />
