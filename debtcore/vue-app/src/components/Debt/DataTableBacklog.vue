c4453b64131a00d014297900ff98919621f5e1ac
weeiwaai <weeiwaai02@gmail.com>
2024-06-12 00:49:55 +0930

    backlog

--- debtcore/vue-app/src/components/Debt/DataTableBacklog.vue
+++ debtcore/vue-app/src/components/Debt/DataTableBacklog.vue
@@ -9,7 +9,7 @@ import {
 	SheetClose
 } from '@/components/ui/sheet'
 import { Button } from '@/components/ui/button'
-import { ref, inject, reactive, onMounted, nextTick, type Ref } from 'vue'
+import { ref, inject, reactive, onMounted, nextTick, watch, type Ref } from 'vue'
 import axios from 'axios'
 import { format, parseISO } from 'date-fns'
 import { useAuthStore } from '@/store/user'
@@ -66,6 +66,13 @@ async function init() {
 
 		is_loading.value = false
 	} catch (error) {
+		let errorMessage = 'Form initialisation error.'
+		toast({
+			title: 'Whoops, something went wrong',
+			description: errorMessage || '',
+			variant: 'destructive'
+		})
+	} finally {
 		is_loading.value = false
 	}
 }
@@ -130,10 +137,12 @@ const updateHeight = () => {
 				wrapperHeight.value -
 				((headerElement.value?.clientHeight || 0) +
 					(footerElement.value?.clientHeight || 0)) -
-				25
+				50
 		}
 	})
 }
+
+watch([headerElement, footerElement], updateHeight, { immediate: true })
 
 function toggleSheet() {
 	is_dialog_open.value = !is_dialog_open.value
@@ -165,54 +174,56 @@ function toggleSheet() {
 				</div>
 				<Separator class="mt-2 mb-4 border-primary border" />
 
-				<ScrollArea class="flex flex-col" :style="{ height: bodyElement + 'px' }">
-					<div class="pr-4">
-						<div v-for="(msg, index) in messages" :key="index" class="pr-4 my-2">
-							<div class="flex justify-between">
-								<div
-									class="text-primary font-bold flex place-items-center self-center"
-									v-if="msg.is_system_generated"
-								>
-									<VIcon
-										name="bi-circle-fill"
-										class="size-3 my-auto fill-green-600 mr-2"
-										animation="pulse"
-									/>
-									System Generated
+				<div class="flex flex-col justify-between">
+					<ScrollArea class="flex flex-col h-full" :style="{ height: '85vh' }">
+						<div class="pr-4">
+							<div v-for="(msg, index) in messages" :key="index" class="pr-4 my-2">
+								<div class="flex justify-between">
+									<div
+										class="text-primary font-bold flex place-items-center self-center"
+										v-if="msg.is_system_generated"
+									>
+										<VIcon
+											name="bi-circle-fill"
+											class="size-3 my-auto fill-green-600 mr-2"
+											animation="pulse"
+										/>
+										System Generated
+									</div>
+									<div class="text-primary font-bold" v-else>
+										{{ msg.created_by_name }}
+									</div>
 								</div>
-								<div class="text-primary font-bold" v-else>
-									{{ msg.created_by_name }}
-								</div>
-							</div>
 
-							<div>
-								<div class="text-primary">
-									{{ msg.created_date }}
-								</div>
-								<Separator variant="default" />
+								<div>
+									<div class="text-primary">
+										{{ msg.created_date }}
+									</div>
+									<Separator variant="default" />
 
-								<div>{{ msg.message }}</div>
+									<div>{{ msg.message }}</div>
+								</div>
 							</div>
 						</div>
-					</div>
-				</ScrollArea>
+					</ScrollArea>
 
-				<div class="flex flex-col" ref="footerElement">
-					<Label for="error" class="text-red-600 flex" v-if="error_message">
-						<VIcon name="fa-exclamation-triangle" class="size-4 fill-red-600" />
-						<p class="text-sm">{{ error_message }}</p>
-					</Label>
-					<div class="flex mt-2 gap-2">
-						<Input
-							id="message"
-							v-model="form.message"
-							placeholder="Enter your message here"
-							class="w-full flex-1"
-						/>
+					<div class="flex flex-col" ref="footerElement">
+						<Label for="error" class="text-red-600 flex" v-if="error_message">
+							<VIcon name="fa-exclamation-triangle" class="size-4 fill-red-600" />
+							<p class="text-sm">{{ error_message }}</p>
+						</Label>
+						<div class="flex mt-2 gap-2">
+							<Input
+								id="message"
+								v-model="form.message"
+								placeholder="Enter your message here"
+								class="w-full flex-1"
+							/>
 
-						<Button type="submit" @click="submit"> Save </Button>
+							<Button type="submit" @click="submit"> Save </Button>
+						</div>
+						<p class="text-xs mt-2">{{ auth.getUsername }}</p>
 					</div>
-					<p class="text-xs mt-2">{{ auth.getUsername }}</p>
 				</div>
 			</SheetContent>
 		</Sheet>
